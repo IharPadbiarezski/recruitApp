041e7ea7d5ff4ec9621571dfbd8e9c8430f66fb6
Mikhail Padbiarezski <mikhailpodberezskyk@gmail.com>
2023-09-12 03:33:10 +0300

    Install Style Dictionary

---
+++ client/tokens/build/ios/StyleDictionarySize.h
@@ -0,0 +1,15 @@
+
+// StyleDictionarySize.h
+//
+
+// Do not edit directly
+// Generated on Tue, 12 Sep 2023 00:26:31 GMT
+
+
+#import <Foundation/Foundation.h>
+
+
+extern float const SizeFontSmall;
+extern float const SizeFontMedium;
+extern float const SizeFontLarge;
+extern float const SizeFontBase;
