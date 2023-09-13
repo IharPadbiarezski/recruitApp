1603ab6b2529a656a021aea275bdc457b92c6e7e
Mikhail Padbiarezski <mikhailpodberezskyk@gmail.com>
2023-09-14 02:36:39 +0300

    fix: import path for _variables.css

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import '/Users/mihailpodberezskij/Documents/Dev/recruitApp/client/src/text_tokens/build/css/_variables.css'
+import './text_tokens/build/css/_variables.css'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
