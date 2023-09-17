9e450a2aee3c92539b986b7318b98eda4b643109
IharPadbiarezski <yb.igrok.by@gmail.com>
2023-09-17 11:21:04 +0300

    feat: create sidebar

--- client/src/components/SidebarItem/SidebarItem.jsx
+++ client/src/components/SidebarItem/SidebarItem.jsx
@@ -4,10 +4,10 @@ import './SidebarItem.css'
 
 
 const SidebarItem = ( { item }) => {
-const { to, d, id } = item
+const { to, d} = item
 
   return (
-    <Link className='sidebar-item-container' key={id} to={to}>
+    <Link className='sidebar-item-container' to={to}>
             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="25" viewBox="0 0 24 25" fill="#363F43" fillOpacity="0.6">
                 <path d={d} />
             </svg>
